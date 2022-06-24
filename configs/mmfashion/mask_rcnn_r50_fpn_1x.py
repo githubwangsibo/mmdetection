adc1f7456c42cb6a7a07c9e7a4dcbe288e0b9cd2
Legion 7i <wangsibo2013@gmail.com>
2022-06-25 05:22:34 +0800

    Update mask_rcnn_r50_fpn_1x.py

--- configs/mmfashion/mask_rcnn_r50_fpn_1x.py
+++ configs/mmfashion/mask_rcnn_r50_fpn_1x.py
@@ -151,7 +151,12 @@ test_pipeline = [
             dict(type='Normalize', **img_norm_cfg),
             dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
+            dict(
+                type='Collect', 
+                keys=['img'], 
+                meta_keys=('filename', 'ori_shape',
+                        'img_shape', 'pad_shape', 'scale_factor', 'flip',
+                        'flip_direction', 'img_norm_cfg')),
         ])
 ]
 
